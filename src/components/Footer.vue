1b6b21c0f8403df348218841ac71f8ddf5858270
Jen Looper <jen.looper@gmail.com>
2020-03-24 15:34:43 -0400

    styling changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,20 @@
 <template>
-	<footer class="footer has-background-primary">
-		<section class="has-text-centered">
-			<h4 class="has-text-white">&copy; Matthew Looper 2016-present</h4>
-		</section>
-	</footer>
+  <footer class="footer has-background-primary">
+    <section class="has-text-centered">
+      <h4>&copy; Matthew Looper 2016-present</h4>
+    </section>
+  </footer>
 </template>
 
 <style scoped>
-/*footer {
-	bottom: -10px;
-	left: 0;
-	position: fixed;
-	right: 0;
-	z-index: 30;
+footer {
+  bottom: -10px;
+  left: 0;
+  position: fixed;
+  right: 0;
+  z-index: 30;
 }
 footer p {
-	padding: 5px;
-}*/
+  padding: 5px;
+}
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,9 @@
   <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="./">
-        <span class="navbar-item is-size-3 has-text-white">Maya Hieroglyphic Database and Archive</span>
+        <span class="navbar-item logo">
+          <img src="/images/logo.png" />
+        </span>
       </a>
       <a
         id="burger"
@@ -21,13 +23,13 @@
     <div id="menu" class="navbar-menu">
       <div class="navbar-end">
         <div class="navbar-item">
-          <router-link class="navbar-item has-text-white" :to="'about'">About</router-link>
-          <span v-if="!$auth.isAuthenticated">
+          <router-link class="navbar-item" :to="'about'">About</router-link>
+          <span v-if="!$auth.isAuthenticated && loadAuth">
             <button id="qsLoginBtn" class="button is-warning" @click.prevent="login">Login</button>
           </span>
 
           <span v-if="$auth.isAuthenticated">
-            <router-link :to="'profile'" class="navbar-item has-text-white">Profile</router-link>
+            <router-link :to="'profile'" class="navbar-item">Profile</router-link>
             <button id="qsLogoutBtn" class="button is-warning" @click.prevent="logout">Logout</button>
           </span>
         </div>
@@ -37,33 +39,57 @@
 </template>
 
 <script web>
-import { mapState } from "vuex";
+//import { mapState } from "vuex";
+import { getInstance } from "../auth/authWrapper";
 
 export default {
   name: "my-navbar",
   computed: {
-    ...mapState(["user"])
+    //...mapState(["user"])
   },
-
+  data() {
+    return {
+      loadAuth: false
+    };
+  },
   methods: {
     login() {
       this.$auth.loginWithRedirect().then(res => {
-        console.log(res);
         this.$router.push({ path: "/profile" });
       });
     },
     logout() {
       this.$auth.logout();
       this.$router.push({ path: "/" });
+    },
+    init(fn) {
+      // have to do this nonsense to make sure auth0Client is ready
+      var instance = getInstance();
+      instance.$watch("loading", loading => {
+        if (loading === false) {
+          fn(instance);
+        }
+      });
+    },
+    async checkAuth(instance) {
+      await instance.getTokenSilently().then(authToken => {
+        // eslint-disable-next-line no-console
+        console.log(authToken);
+        this.loadAuth = true;
+        // do authorized API calls with auth0 authToken here
+      });
     }
   },
-  mounted: function() {
+  mounted() {
     var burger = document.getElementById("burger");
     var menu = document.getElementById("menu");
     burger.addEventListener("click", function() {
       burger.classList.toggle("is-active");
       menu.classList.toggle("is-active");
     });
+  },
+  created() {
+    this.init(this.checkAuth);
   }
 };
 </script>
@@ -83,5 +109,9 @@ export default {
 
 a.navbar-item:hover {
   background-color: transparent;
+}
+
+.navbar-item img {
+  min-height: 4rem;
 }
 </style>
